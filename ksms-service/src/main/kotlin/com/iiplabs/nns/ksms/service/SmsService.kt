e235c288a940687f8e84142961b82580ba88f59e
Igor Ilyutkin <igor@iiplabs.com>
2022-12-21 15:36:24 -0400

    ksms-service - logging updates

--- ksms-service/src/main/kotlin/com/iiplabs/nns/ksms/service/SmsService.kt
+++ ksms-service/src/main/kotlin/com/iiplabs/nns/ksms/service/SmsService.kt
@@ -1,23 +1,23 @@
 package com.iiplabs.nns.ksms.service
 
+import mu.KotlinLogging
 import kotlinx.coroutines.delay
 import org.springframework.stereotype.Service
 
 import com.iiplabs.nns.ksms.model.SendMessageRequestDto
-import org.apache.log4j.spi.LoggerFactory
 import java.time.Duration.ofSeconds
 
 interface SmsService {
     fun send(sendMessageRequestDto: SendMessageRequestDto)
 }
 
+private val logger = KotlinLogging.logger {}
+
 @Service
 class SmsServiceImpl : SmsService {
 
     companion object {
         private val MAX_EXECUTION_DELAY = 5L
-
-        private val logger = LoggerFactory.getLogger(SmsServiceImpl::class.java)
     }
 
     override fun send(sendMessageRequestDto: SendMessageRequestDto) {
@@ -30,7 +30,7 @@ class SmsServiceImpl : SmsService {
         val randomDelayLong = (1..MAX_EXECUTION_DELAY).shuffled().first().toLong()
         delay(ofSeconds(randomDelayLong))
 
-        logger.info("Success sending SMS to {}, with delay {} seconds", sendMessageRequestDto.destinationPhone, randomDelayLong);
+        logger.info { "Success sending SMS to ${sendMessageRequestDto.destinationPhone}, with delay ${randomDelayLong} seconds" }
     }
 
 }
